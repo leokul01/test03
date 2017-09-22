3df3eed4da0979e64cf8963262d679543873cce1
Leonid Kulikov <leokul01@students.programming1189.ru>
2017-09-22 14:43:36 +0300

    It's done!

--- pascal.cpp
+++ pascal.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
-
+int fact(int p) {
+	if (p<2) return 1;
+	return p*fact(p-1);
+    }
 int main()
 {
     // TODO: написать вывод треугольника Паскаля с использованием vector.
@@ -10,9 +13,28 @@ int main()
     // 1
     // 1 1
     // 1 2 1
-    // 1 3 3 1
-    // 1 4 6 4 1
+    // 1 3 3 1// 1 4 6 4 1
     // ...
+    
+    
+    vector<int> v;
+    int count =0,place = 0;
+    v.push_back(1);
+    cout << '1' << endl;
+
+    for(int line=1;line<=10;line++) {
+	for(int j = 1;j<=line;j++) {
+	    v.push_back(fact(line)/(fact(j)*fact(line-j)));
+	    count++;    
+	}
+	for(int j=place;j<=place + count;j++) {
+	    cout << v[j] << ' ';
+	}
+	place+=count;
+	count = 0;
+	cout << endl;
+    }
+    
     return 0;
 }
 
