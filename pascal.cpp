579c8c940b1ae7f90f308420fc71cc98ebf6cd65
Leonid Kulikov <leokul01@students.programming1189.ru>
2017-09-24 14:15:05 +0300

    I've modified algorithm. Now it's more effective!

--- pascal.cpp
+++ pascal.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int fact(int p) {
-	if (p<2) return 1;
-	return p*fact(p-1);
-    }
 int main()
 {
     // TODO: написать вывод треугольника Паскаля с использованием vector.
@@ -13,26 +9,27 @@ int main()
     // 1
     // 1 1
     // 1 2 1
-    // 1 3 3 1// 1 4 6 4 1
-    // ...
-    
-    
-    vector<int> v;
-    int count =0,place = 0;
-    v.push_back(1);
-    cout << '1' << endl;
+    // 1 3 3 1
+    // 1 4 6 4 1
+    // ...        
 
-    for(int line=1;line<=10;line++) {
-	for(int j = 1;j<=line;j++) {
-	    v.push_back(fact(line)/(fact(j)*fact(line-j)));
-	    count++;    
-	}
-	for(int j=place;j<=place + count;j++) {
-	    cout << v[j] << ' ';
-	}
-	place+=count;
-	count = 0;
-	cout << endl;
+    cout << "input n - number of strings in triangle" << '\n';
+    int n;
+    cin >> n;
+
+    vector<vector<int> > v(n, vector<int>());
+
+    for(int i=0;i<n;i++) {
+	v[i].push_back(1);
+	for(int j=1;j<i;j++) 
+	    v[i].push_back(v[i-1][j-1]+v[i-1][j]);
+	v[i].push_back(1);
+    }
+
+    for(int i=0;i<n;i++) {
+	for(int j=0;j<=i;j++) 
+	    cout << v[i][j] << ' ';
+	cout << '\n';
     }
     
     return 0;
